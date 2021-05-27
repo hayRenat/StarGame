bdba48003136ebad654769aea531e2ad2ebbec47
hayrenat <hayrenat@gmail.com>
2021-05-27 19:03:56 +0300

    Create Game Project

---
+++ desktop/src/ru/hayrenat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.hayrenat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.hayrenat.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
